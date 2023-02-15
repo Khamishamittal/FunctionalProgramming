d8fe6081d5cb43946465f38d89c69e2d50b9cfbe
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 14:39:36 +0530

    Print Power of 2

---
+++ PowerOf2.cs
@@ -0,0 +1,28 @@
+﻿namespace FunctionalProgramming
+{
+    public class PowerOf2
+    {
+        public static void PrintPowerOf2()
+        {
+            int N = 2;
+            int result = 1;
+
+            Console.Write("Enter a Power number: ");
+            int power = Convert.ToInt32(Console.ReadLine());
+            if (power >= 0 && power < 31)
+            {
+                for (int i = 0; i < power; i++)
+                {
+                    Console.Write((result = result * N) + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("enter a power between 0 and 31");
+            }
+        }
+    }
+}
+   
+
