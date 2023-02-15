9cc9fd1a31c1fd744ef41b2ff8f6b3534dcc2dc6
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 16:41:36 +0530

    Check Year is Leap Year or not

---
+++ LeapYear.cs
@@ -0,0 +1,24 @@
+﻿ namespace LogicalProgramming
+    {
+        public class LeapYear
+        {
+            public static void CheckLeapYear()
+            {
+                Console.WriteLine("Enter a 4 digit year to check for leap year");
+
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                {
+                    Console.WriteLine(year + " is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine(year + " is not a leap year");
+                }
+            }
+
+        }
+    }
+    
+
