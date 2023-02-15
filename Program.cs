81ad6c1d2ef049222ac11233ffb7064682f4496a
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 19:55:09 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using FunctionalProgramming;
+using LogicalProgramming;
+
+namespace FunctionalProgrammimg
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //QuotientAndRemainder.FindQuotientAndRemainder();
+            //EvenOrOdd.FindNumber();
+            //VowelAndConsonant.CheckTheCharacter();
+            // Swap.SwapingTheNumber();
+            //LargestNumbers.FindTheLargestNumber();
+            // PowerOf2.PrintPowerOf2();
+            // PrimeFactors.CheckPrimeFactors();
+            //  LeapYear.CheckLeapYear();
+            FlipCoin.CalculatePercentage();
+
+        }
+    }
+}
+
