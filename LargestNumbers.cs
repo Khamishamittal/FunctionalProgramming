ce1f9f09a8e443b14d8ea49e0c995f4bf7841aca
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 13:47:04 +0530

    Find the Largest Number among three numbers

---
+++ LargestNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace FunctionalProgramming
+{
+    public class LargestNumbers
+    {
+        public static void FindTheLargestNumber()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Find the largest of three Number");
+            Console.WriteLine("Input the Ist Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the 2nd number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the 3rd number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("The 1 number is greatest among three");
+                }
+                else
+                {
+                    Console.WriteLine("The 3 number is greatest among three");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("The 2 number is greatest among three");
+            }
+            else
+            {
+                Console.WriteLine("The 3 number is greatest among three");
+
+            }
+        }
+    }
+}
+
+
