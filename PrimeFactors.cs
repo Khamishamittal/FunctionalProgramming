56eae01d7e1a01c3c36a5ac7bef9db3576f4f36f
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 14:48:10 +0530

    Compute Prime Factors of a number

---
+++ PrimeFactors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class PrimeFactors
+    {
+        public static void CheckPrimeFactors()
+        {
+            Console.WriteLine("Enter a Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i * i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n = n / i;
+                }
+            }
+            if (n > 1)
+            {
+                Console.Write(n);
+            }
+        }
+    }
+}
+
