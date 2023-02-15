37341b3d7fc6ed7a88ce327b7c4e168101e6626f
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-15 14:12:59 +0530

    Find Quotient and Remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class QuotientAndRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int dividend = 20,  divisor = 10;
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Dividend : {0} Divisor : {1}",dividend ,divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+
+        }
+    }
+}
