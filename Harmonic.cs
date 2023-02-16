4b4845ad92fa03367bd84de48b45bf98fd65a356
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-16 21:41:22 +0530

    Print Harmonic Series

---
+++ Harmonic.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace FunctionalProgramming
+{
+    public class Harmonic
+    {
+        public static void PrintHarmonic()
+            {
+                Console.Write("Enter a harmonic number: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                if (N != 0)
+                {
+                    for (int i = 1; i <= N; i++)
+                    {
+                        Console.Write("1/" + i + " ");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid number ");
+                }
+            }
+        }
+    }
+
+
+
